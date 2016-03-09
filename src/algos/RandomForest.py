277f9c06e7bdc1e511c86abb6c0f4406d922a77a
Alexey Polbitsyn <Fob.0xFF@gmail.com>
2016-03-09 21:57:55 +0300

    random forest with new features

--- src/algos/RandomForest.py
+++ src/algos/RandomForest.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
 from src.algos.utils import rmse
 from src.features.features2strmatrix import load_features
 
 X_train, y_train, X_test, id_train, id_test = load_features()
 
-clf = LinearRegression()
+clf = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=257)
 clf.fit(X_train, y_train)
 
 y_predicted = clf.predict(X_train)
@@ -19,6 +19,5 @@ y_test[y_test < 1] = 1
 y_test[y_test > 3] = 3
 
 out = pd.DataFrame({'id': id_test, 'relevance': y_test})
-out.to_csv('./result/linear_regression.csv', index=None)
-# 0.52233
-# 0.51889
+out.to_csv('./result/random_forest_regression.csv', index=None)
+# 0.52430
