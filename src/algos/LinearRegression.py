72796cdec855623f755b3d11c0c39f4c5c653d07
Alexey Polbitsyn <Fob.0xFF@gmail.com>
2016-03-09 22:57:21 +0300

    random forest with new features

--- src/algos/LinearRegression.py
+++ src/algos/LinearRegression.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
 
 from src.algos.utils import rmse
 from src.features.features2strmatrix import load_features
 
 X_train, y_train, X_test, id_train, id_test = load_features()
 
-clf = LinearRegression()
+clf = linear_model.Ridge(alpha=0.02484, normalize=True)
 clf.fit(X_train, y_train)
 
 y_predicted = clf.predict(X_train)
@@ -19,6 +19,7 @@ y_test[y_test < 1] = 1
 y_test[y_test > 3] = 3
 
 out = pd.DataFrame({'id': id_test, 'relevance': y_test})
-out.to_csv('./result/linear_regression.csv', index=None)
+out.to_csv('./result/linear_regression_regularized.csv', index=None)
 # 0.52233
 # 0.51889
+# 0.51883
