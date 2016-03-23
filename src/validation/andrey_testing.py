308ce0754d254002c2c8dabbf95b977c59bdeda8
andre-vk <andre_mp@mail.ru>
2016-03-23 07:53:22 +0300

    word2vec

--- src/validation/andrey_testing.py
+++ src/validation/andrey_testing.py
@@ -15,23 +15,24 @@ w2v_wiki_model = w2v.read_w2v_model_from_text_file("/media/andrey/ARCHIVE/w2v_tr
 # ADD Word2Vec Columns
 train_data = pd.read_csv('./dataset/train_no_stem_no_sw.csv', index_col='id')
 
+w2v_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc(w2v_wiki_model, train_data)
+
 # Cross-Validation
 scores = []
 thresholds = np.arange(5,100,5)/100.
 cv = KFold(n=len(train_df), n_folds=10, shuffle=True, random_state=23)
-for threshold in [0.5]:
+for threshold in [0.05]:
 
-    final_train_df = train_df
+    final_train_df = train_df.copy()
     final_train_df.iloc[10]
 
-    #w2v_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc(w2v_wiki_model, train_data)
-    #train_df[w2v_doc_vect_features_train.columns.values] = w2v_doc_vect_features_train
+    final_train_df[w2v_doc_vect_features_train.columns.values] = w2v_doc_vect_features_train
 
-    w2v_count_synonyms_features_train = w2v.get_features_count_synonyms(w2v_wiki_model, train_data, threshold)
-    final_train_df[w2v_count_synonyms_features_train.columns.values] = w2v_count_synonyms_features_train
+    #w2v_count_synonyms_features_train = w2v.get_features_count_synonyms(w2v_wiki_model, train_data, threshold)
+    #final_train_df[w2v_count_synonyms_features_train.columns.values] = w2v_count_synonyms_features_train
 
-    X_train = final_train_df.ix[:, final_train_df.columns != 'relevance'].values
-    y_train = train_df['relevance'].values
+    X_train = final_train_df.drop('relevance', axis=1).values
+    y_train = final_train_df['relevance'].values
 
     clf = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=42, max_depth=10, max_features=4)
     score = cross_val_score(clf, X_train, y_train, scoring=RMSE, cv=cv)
@@ -41,27 +42,36 @@ for threshold in [0.5]:
 
 
 # TRAIN ALL
-#w2v_features_train = w2v.get_features_count_synonyms(w2v_wiki_model, train_data, 0.1)
-#train_df[w2v_features_train.columns.values] = w2v_features_train
-final_train_df = train_df
-X_train = final_train_df.ix[:, final_train_df.columns != 'relevance'].values
-y_train = train_df['relevance'].values
+#----------------------------------------------------
+final_train_df = train_df.copy()
+
+#w2v_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc(w2v_wiki_model, train_data)
+final_train_df[w2v_doc_vect_features_train.columns.values] = w2v_doc_vect_features_train
+
+threshold = 0.05
+w2v_count_synonyms_features_train = w2v.get_features_count_synonyms(w2v_wiki_model, train_data, threshold)
+final_train_df[w2v_count_synonyms_features_train.columns.values] = w2v_count_synonyms_features_train
+
+X_train = final_train_df.drop('relevance', axis=1).values
+y_train = final_train_df['relevance'].values
 clf = RandomForestRegressor(n_estimators=500, n_jobs=-1, random_state=42, max_depth=10, max_features=4)
 clf.fit(X_train, y_train)
 np.sqrt((1.0/len(y_train))*np.sum((y_train-clf.predict(X_train))**2))
+#----------------------------------------------------
 
 
 # TEST OUTPUT
+#----------------------------------------------------
 test_data = pd.read_csv('./dataset/test_no_stem_no_sw.csv', index_col='id')
+final_test_df = test_df.copy()
 
-w2v_dov_vect_features_test = w2v.get_features_sum_of_vects_as_doc(w2v_wiki_model, test_data)
-w2v_count_synonyms_features_test = w2v.get_features_count_synonyms(w2v_wiki_model, test_data, 0.1)
-test_df[w2v_dov_vect_features_test.columns.values] = w2v_dov_vect_features_test
-test_df[w2v_count_synonyms_features_test.columns.values] = w2v_count_synonyms_features_test
+w2v_doc_vect_features_test = w2v.get_features_sum_of_vects_as_doc(w2v_wiki_model, test_data)
+final_test_df[w2v_doc_vect_features_test.columns.values] = w2v_doc_vect_features_test
+w2v_count_synonyms_features_test = w2v.get_features_count_synonyms(w2v_wiki_model, test_data, threshold)
+final_test_df[w2v_count_synonyms_features_test.columns.values] = w2v_count_synonyms_features_test
 
-
-final_test_df = test_df
 X_test = final_test_df.values
 y_test = clf.predict(X_test)
 out = pd.DataFrame({'id': test_df.index, 'relevance': y_test})
-out.to_csv('./result/w2v_wiki_test_syn_and_doc_0_1.csv', index=None)
+out.to_csv('./result/w2v_wiki_test_docandcos_thr_0_05.csv', index=None)
+#----------------------------------------------------
