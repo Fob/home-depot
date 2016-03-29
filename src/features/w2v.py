73684276d536efee4baf62530c1680ff96b486ba
andre-vk <andre_mp@mail.ru>
2016-03-29 08:57:14 +0300

    word2vec

--- src/features/w2v.py
+++ src/features/w2v.py
@@ -66,76 +66,104 @@ def read_w2v_model_from_text_file(filename):
     return w2v
 
 
-def get_features_sum_of_vects_as_doc(w2v_model, data):
 
-    features = pd.DataFrame({'sim_with_title_w2v': 0, 'sim_with_descr_w2v': 0}, index=data.index)
-    for n in data.index:
+
+def get_features_sum_of_vects_as_doc_fast_method(w2v_model, data):
+
+    def get_cosine_value(search, title, descr, n):
+
         if n % 10000 == 0:
-            print "%d rows of %d processed" % (n, data.last_valid_index())
-        row_n = data.loc[n]
-        descr = set(row_n['descr'].split())
-        title = set(row_n['product_title'].split())
-        search = row_n['search_term'].split()
+            print "%d rows processed" % (n)
 
+        search = search.split()
+        title = title.split()
+        descr = descr.split()
 
         #search vector
-        if sum([term in (w2v_model) for term in search]) == 0:
+        if sum([term in (shared_w2v_model) for term in search]) == 0:
             sim_with_title_w2v = 0
             sim_with_descr_w2v = 0
         else:
-            vectors = [w2v_model[term] for term in search if term in w2v_model]
+            vectors = [shared_w2v_model[term] for term in search if term in shared_w2v_model]
             search_vector = sum(vectors, 0)/len(vectors)
             #search_vector = sum([w2v_model[term] for term in search])/len(search)
 
             #title_vector
-            if sum([term in (w2v_model) for term in title]) == 0:
+            if sum([term in (shared_w2v_model) for term in title]) == 0:
                 sim_with_title_w2v = 0
             else:
-                vectors = [w2v_model[term] for term in title if term in w2v_model]
+                vectors = [shared_w2v_model[term] for term in title if term in shared_w2v_model]
                 title_vector = sum(vectors, 0)/len(vectors)
                 sim_with_title_w2v = np.dot(search_vector, title_vector)/(la.norm(search_vector)*la.norm(title_vector))
 
             #descr_vector
-            if sum([term in (w2v_model) for term in descr]) == 0:
+            if sum([term in (shared_w2v_model) for term in descr]) == 0:
                 sim_with_descr_w2v = 0
             else:
-                vectors = [w2v_model[term] for term in descr if term in w2v_model]
+                vectors = [shared_w2v_model[term] for term in descr if term in shared_w2v_model]
                 descr_vector = sum(vectors, 0)/len(vectors)
                 sim_with_descr_w2v = np.dot(search_vector, descr_vector)/(la.norm(search_vector)*la.norm(descr_vector))
 
 
-        features.loc[n] = np.array([sim_with_descr_w2v, sim_with_title_w2v])
+        return np.array([sim_with_title_w2v, sim_with_descr_w2v])
 
-    print "Done!"
-
+    global shared_w2v_model
+    shared_w2v_model = w2v_model
+    result = map(lambda search, title, descr, n: get_cosine_value(search, title, descr, n), data['search_term'], data['product_title'], data['descr'], range(0,len(data)))
+    features = pd.DataFrame(result, columns=['sim_with_title_w2v', 'sim_with_descr_w2v'], index=data.index)
     return features
-
 
 
 
 def get_features_count_synonyms(w2v_model, data, threshold):
 
-    features = pd.DataFrame({'words_in_title_w2v': 0, 'words_in_descr_w2v': 0, 'ratio_in_title_w2v': 0, 'ratio_in_descr_w2v': 0}, index=data.index)
-    for n in data.index:
-        if n % 10000 == 0:
-            print "%d rows of %d processed" % (n, data.last_valid_index())
-        row_n = data.loc[n]
-        descr = set(row_n['descr'].split())
-        title = set(row_n['product_title'].split())
-        search = row_n['search_term'].split()
+    def get_cosine_value(search, title, descr, n):
+
+        if n % 1000 == 0:
+            print "%d rows processed" % (n)
+
+        descr = descr.split()
+        title = title.split()
+        search = search.split()
 
         title_count_value = 0
         descr_count_value = 0
         for search_term in search:
             #title
-            if search_term in w2v_model:
-                title_count_value = title_count_value + sum([cos_sim(w2v_model[search_term], w2v_model[term])>threshold for term in title if ((term in w2v_model) & (term!=search_term))])
+            if search_term in shared_w2v_model:
+                title_count_value = title_count_value + sum([cos_sim(shared_w2v_model[search_term], shared_w2v_model[term])>threshold for term in title if ((term!=search_term) & (term in shared_w2v_model))])
             #description
-            if search_term in w2v_model:
-                descr_count_value = descr_count_value + sum([cos_sim(w2v_model[search_term], w2v_model[term])>threshold for term in descr if ((term in w2v_model) & (term!=search_term))])
+            if search_term in shared_w2v_model:
+                descr_count_value = descr_count_value + sum([cos_sim(shared_w2v_model[search_term], shared_w2v_model[term])>threshold for term in descr if ((term!=search_term) & (term in shared_w2v_model))])
 
-        features.loc[n] = np.array([ np.double(descr_count_value)/len(search), np.double(title_count_value)/len(search), descr_count_value, title_count_value])
+        return np.array([title_count_value, np.double(title_count_value)/len(search), descr_count_value, np.double(descr_count_value)/len(search)])
+
+    global shared_w2v_model
+    shared_w2v_model = w2v_model
+    result = map(lambda search, title, descr, n: get_cosine_value(search, title, descr, n), data['search_term'], data['product_title'], data['descr'], range(0, len(data)))
+    features = pd.DataFrame(result, columns=['words_in_title_w2v', 'ratio_in_title_w2v', 'words_in_descr_w2v', 'ratio_in_descr_w2v'], index=data.index)
 
     print "Done!"
-
     return features
+
+
+
+
+def get_count_of_words(data):
+
+
+    def get_cnt_value(title, descr, n):
+        if n % 10000 == 0:
+            print "%d rows processed" % (n)
+        descr = descr.split()
+        title = title.split()
+        return np.array([ len(title), len(descr)])
+
+    result = map(lambda title, descr, n: get_cnt_value(title, descr, n), data['product_title'], data['descr'], range(0,len(data)))
+    features = pd.DataFrame(result, columns=['cnt_in_title', 'cnt_in_descr'], index=data.index)
+
+    print "Done!"
+    return features
+
+
+
--- src/validation/andrey_testing.py
+++ src/validation/andrey_testing.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import src.features.extract_count_features as ext_c_ft
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.cross_validation import KFold
@@ -8,25 +9,42 @@ import src.features.w2v as w2v
 import pandas as pd
 
 
-train_df, test_df = ext_c_ft.load_features1('features_size')
+#train_df, test_df = ext_c_ft.load_features1('features_size')
+
+train_df = pd.read_csv('./src/features/builded_from_loadfeztures1_train.csv', index_col='id')
+
 
 # load W2V model
 w2v_wiki_model = w2v.read_w2v_model_from_text_file("/media/andrey/ARCHIVE/w2v_trained/vectors_wiki1B_skipgram_s500_w5_neg20_hs0_sam1e-4_iter5.txt")
+# load W2V model
+w2v_descr_title_model = w2v.read_w2v_model_from_text_file("/home/andrey/Kaggle/home-depot/dataset/all_text_w2v_model")
 # ADD Word2Vec Columns
 train_data = pd.read_csv('./dataset/train_no_stem_no_sw.csv', index_col='id')
 
-w2v_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc(w2v_wiki_model, train_data)
+w2v_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc_fast_method(w2v_wiki_model, train_data)
+
+w2v_title_descr_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc_fast_method(w2v_descr_title_model, train_data)
+w2v_title_descr_doc_vect_features_train.columns = w2v_title_descr_doc_vect_features_train.columns.values+'_title_descr'
+
+cnt_in_titledescr = w2v.get_count_of_words(train_data)
 
 # Cross-Validation
 scores = []
-thresholds = np.arange(5,100,5)/100.
-cv = KFold(n=len(train_df), n_folds=10, shuffle=True, random_state=23)
-for threshold in [0.05]:
+thresholds = [0]
+#thresholds = np.arange(10,60,10)/100.
+#thresholds = [0.01, 0.02, 0.03, 0.04, 0.05]
+cv = KFold(n=len(train_df), n_folds=5, shuffle=True, random_state=23)
+for threshold in thresholds:
 
     final_train_df = train_df.copy()
     final_train_df.iloc[10]
 
+
     final_train_df[w2v_doc_vect_features_train.columns.values] = w2v_doc_vect_features_train
+
+    final_train_df[w2v_title_descr_doc_vect_features_train.columns.values] = w2v_title_descr_doc_vect_features_train
+
+    final_train_df[cnt_in_titledescr.columns.values] = cnt_in_titledescr
 
     #w2v_count_synonyms_features_train = w2v.get_features_count_synonyms(w2v_wiki_model, train_data, threshold)
     #final_train_df[w2v_count_synonyms_features_train.columns.values] = w2v_count_synonyms_features_train
@@ -39,39 +57,84 @@ for threshold in [0.05]:
     scores = np.append(scores, score.mean())
     print 'CV score = %f for %f threshold' % (score.mean(), threshold)
 
+plt.plot(np.array(thresholds), scores)
+plt.show()
 
+out = pd.DataFrame({'scores': scores})
+out.to_csv('./result/scores.csv', index=None)
 
 # TRAIN ALL
 #----------------------------------------------------
 final_train_df = train_df.copy()
 
-#w2v_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc(w2v_wiki_model, train_data)
+w2v_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc_fast_method(w2v_wiki_model, train_data)
 final_train_df[w2v_doc_vect_features_train.columns.values] = w2v_doc_vect_features_train
 
-threshold = 0.05
-w2v_count_synonyms_features_train = w2v.get_features_count_synonyms(w2v_wiki_model, train_data, threshold)
-final_train_df[w2v_count_synonyms_features_train.columns.values] = w2v_count_synonyms_features_train
+w2v_title_descr_doc_vect_features_train = w2v.get_features_sum_of_vects_as_doc_fast_method(w2v_descr_title_model, train_data)
+w2v_title_descr_doc_vect_features_train.columns = w2v_title_descr_doc_vect_features_train.columns.values+'_title_descr'
+final_train_df[w2v_title_descr_doc_vect_features_train.columns.values] = w2v_title_descr_doc_vect_features_train
+
+cnt_in_titledescr = w2v.get_count_of_words(train_data)
+final_train_df[cnt_in_titledescr.columns.values] = cnt_in_titledescr
+
+#threshold = 0.04
+#w2v_count_synonyms_features_train = w2v.get_features_count_synonyms(w2v_wiki_model, train_data, threshold)
+#final_train_df[w2v_count_synonyms_features_train.columns.values] = w2v_count_synonyms_features_train
 
 X_train = final_train_df.drop('relevance', axis=1).values
 y_train = final_train_df['relevance'].values
 clf = RandomForestRegressor(n_estimators=500, n_jobs=-1, random_state=42, max_depth=10, max_features=4)
 clf.fit(X_train, y_train)
 np.sqrt((1.0/len(y_train))*np.sum((y_train-clf.predict(X_train))**2))
+out = final_train_df[['sim_with_title_w2v', 'sim_with_descr_w2v', 'sim_with_title_w2v_title_descr', 'sim_with_descr_w2v_title_descr', 'cnt_in_title', 'cnt_in_descr']]
+
+out.to_csv('./src/features/w2v_wiki_and_descr_without_typos_train_features.csv', index=None)
 #----------------------------------------------------
 
 
 # TEST OUTPUT
 #----------------------------------------------------
+test_df = pd.read_csv('./src/features/builded_from_loadfeztures1_test.csv', index_col='id')
 test_data = pd.read_csv('./dataset/test_no_stem_no_sw.csv', index_col='id')
 final_test_df = test_df.copy()
 
-w2v_doc_vect_features_test = w2v.get_features_sum_of_vects_as_doc(w2v_wiki_model, test_data)
+#on w2v wiki
+w2v_doc_vect_features_test = w2v.get_features_sum_of_vects_as_doc_fast_method(w2v_wiki_model, test_data)
 final_test_df[w2v_doc_vect_features_test.columns.values] = w2v_doc_vect_features_test
-w2v_count_synonyms_features_test = w2v.get_features_count_synonyms(w2v_wiki_model, test_data, threshold)
-final_test_df[w2v_count_synonyms_features_test.columns.values] = w2v_count_synonyms_features_test
+#on w2v title_descr
+w2v_title_descr_doc_vect_features_test = w2v.get_features_sum_of_vects_as_doc_fast_method(w2v_descr_title_model, test_data)
+w2v_title_descr_doc_vect_features_test.columns = w2v_title_descr_doc_vect_features_test.columns.values+'_title_descr'
+final_test_df[w2v_title_descr_doc_vect_features_test.columns.values] = w2v_title_descr_doc_vect_features_test
+#just count of words
+cnt_in_titledescr_test = w2v.get_count_of_words(test_data)
+final_test_df[cnt_in_titledescr_test.columns.values] = cnt_in_titledescr_test
+#synonyms
+#w2v_count_synonyms_features_test = w2v.get_features_count_synonyms(w2v_wiki_model, test_data, threshold)
+#final_test_df[w2v_count_synonyms_features_test.columns.values] = w2v_count_synonyms_features_test
+
 
 X_test = final_test_df.values
 y_test = clf.predict(X_test)
 out = pd.DataFrame({'id': test_df.index, 'relevance': y_test})
-out.to_csv('./result/w2v_wiki_test_docandcos_thr_0_05.csv', index=None)
+out.to_csv('./result/w2v_wiki_and_descr.csv', index=None)
 #----------------------------------------------------
+
+
+
+
+#----------------------------------------------------
+#TEST NEW FUNCTIONALITY
+#----------------------------------------------------
+train_data = pd.read_csv('./dataset/train_no_stem_no_sw.csv', index_col='id')
+
+
+with open('./dataset/titles.txt', 'a') as f:
+    train_data['product_title'].to_csv(f, index=None, header=False)
+#train_data['product_title'][1:10].to_csv('./dataset/all_text.txt', index=None)
+
+
+w2v_descr_model = w2v.read_w2v_model_from_text_file("/home/andrey/word2vec/home-depot/vectors")
+
+word='makita'
+w2v.cos_sim(w2v_wiki_model[word], w2v_descr_model[word])
+
