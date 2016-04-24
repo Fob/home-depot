6cff4372332a7be57b8c38bd6b70254b7b25f3bb
Alexey Polbitsyn <Fob.0xFF@gmail.com>
2016-04-24 14:35:05 +0300

    keras function

--- src/algos/kerasseq.py
+++ src/algos/kerasseq.py
@@ -16,45 +16,64 @@ program = os.path.basename(sys.argv[0])
 logger = logging.getLogger(program)
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
 logging.root.setLevel(level=logging.INFO)
+
+
 # Main
-features = match_features()
-features_normalized = zero_normalization(features, merge=False)
+
+
+def keras_calc(X_train, y_train, X_test):
+    model = Sequential()
+    # 2 inputs, 10 neurons in 1 hidden layer, with tanh activation and dropout
+    model.add(Dense(300, init='uniform', input_dim=38))
+    model.add(Activation('tanh'))
+    model.add(Dropout(0.50))
+
+    model.add(Dense(300, init='uniform'))
+    model.add(Activation('sigmoid'))
+    model.add(Dropout(0.50))
+
+    model.add(Dense(300, init='uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.50))
+
+    model.add(Dense(300, init='uniform'))
+    model.add(Activation('tanh'))
+    model.add(Dropout(0.50))
+
+
+    # 1 output, linear activation
+    model.add(Dense(1, init='uniform'))
+    model.add(Activation('linear'))
+    model.compile(loss='mse', optimizer='rmsprop')
+
+    model.fit(X_train, y_train, nb_epoch=100, batch_size=32)
+    y_predicted = model.predict(X_train, 32)
+
+    y_predicted[y_predicted < 1] = 1
+    y_predicted[y_predicted > 3] = 3
+    print 'train error', rmse(y_train, y_predicted)
+
+    y_test = model.predict(X_test, 32)
+    y_test[y_test < 1] = 1
+    y_test[y_test > 3] = 3
+
+    return y_test[:, 0]
+
+
+# features = match_features()
+# features_normalized = zero_normalization(features, merge=False)
 # features_validated = select_features('rf.2', features_normalized)
-features_validated = features_normalized
-
-y_train = features_validated['relevance']
-X_train = features_to_x(features_validated)
-
-model = Sequential()
-# 2 inputs, 10 neurons in 1 hidden layer, with tanh activation and dropout
-model.add(Dense(10, init='uniform', input_dim=417))
-model.add(Activation('tanh'))
-model.add(Dropout(0.25))
-# 1 output, linear activation
-model.add(Dense(1, init='uniform'))
-model.add(Activation('linear'))
-model.compile(loss='mse', optimizer='rmsprop')
-
-model.fit(X_train.as_matrix(), y_train, nb_epoch=5, batch_size=32)
-y_predicted = model.predict(X_train.as_matrix(), 32)
-
-y_predicted[y_predicted < 1] = 1
-y_predicted[y_predicted > 3] = 3
-print 'train error', rmse(y_train, y_predicted)
-
-features = match_features(data_file='test')
-features2 = match_features(data_file='train')
-features_normalized = zero_normalization(features, merge=False)
-features_normalized2 = zero_normalization(features2, merge=False)
+train_df = pd.read_csv('./dataset/good_ft_2_train.csv', index_col='id')
+# features = match_features(data_file='test')
+# features_normalized = zero_normalization(features, merge=False)
 # features_validated = select_features('rf.2', features_normalized)
-features_validated = features_normalized
+test_df = pd.read_csv('./dataset/good_ft_2_test.csv', index_col='id')
+y_train = train_df['relevance']
+X_train = features_to_x(train_df)
+X_test = features_to_x(test_df)
 
-X_test = features_to_x(features_validated)
+y_test = keras_calc(X_train.as_matrix(), y_train, X_test.as_matrix())
 
-y_test = model.predict(X_test.as_matrix(), 32)
-y_test[y_test < 1] = 1
-y_test[y_test > 3] = 3
-
-out = pd.DataFrame({'id': features_validated['id'], 'relevance': y_test[:,0]})
+out = pd.DataFrame({'id': X_test.index, 'relevance': y_test})
 out.to_csv('./result/keras.csv', index=None)
 print 'saved ./result/keras.csv'
